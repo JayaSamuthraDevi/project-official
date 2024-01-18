b0b550ade159df3955c12e27d69609cc0fba0ca1
Jaya Samuthra Devi Murugan <murugan.jayasamuthradevi@divum.in>
2024-01-18 20:29:19 +0530

    Initial commit

---
+++ _partials/_head.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title><?php echo isset($title) ? $title : 'Green Makers Media'; ?></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon">
+  <link rel="apple-touch-icon" href="./assets/img/icon.png">
+
+  <!-- ************************* CSS Files ************************* -->
+
+  <!-- Vendor CSS -->
+  <link rel="stylesheet" href="./assets/css/vendor.css">
+
+  <!-- style css -->
+  <link rel="stylesheet" href="./assets/css/main.css">
+</head>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$page = "home";
+$title = "Home | Green Makers Media";
+error_reporting(E_ALL);
+ini_set('display_errors', 1); //showing errors in browser
+include 'libs/load.php';
+load_template('header');
+load_template("footer");
